8d07d486d30f0a4c8bbd479541b120b9d21e8968
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-11-26 10:48:48 +0100

    Toolbar practice

--- TruthOrDrink/Views/NewPage1.xaml.cs
+++ TruthOrDrink/Views/NewPage1.xaml.cs
@@ -6,4 +6,15 @@ public partial class NewPage1 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void HomeToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//main");
+    }
+
+    private async void LogoutToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        await SecureStorage.SetAsync("IsAuthenticated", "false");
+        await Shell.Current.GoToAsync("//login");
+    }
 }
