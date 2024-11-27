6348e0aeb4bcce0b5891fc323c4d79aa8339b70d
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-11-27 20:27:20 +0100

    added new methods

---
+++ TruthOrDrink/Data/TruthOrDrinkDatabase.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruthOrDrink.Models;
+
+namespace TruthOrDrink.Data
+{
+    public class TruthOrDrinkDatabase
+    {
+        SQLiteAsyncConnection Database;
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<Question>();
+        }
+
+        public async Task<List<Question>> GetQuestionsAsync()
+        {
+            await Init();
+
+            return await Database.Table<Question>().ToListAsync();
+        }
+        public async Task<int> UpdateQuestionAsync(Question question)
+        {
+            await Init();
+
+            if (question.Id == 0) // If question does not have an ID then insert a new question in the database.
+            {
+                return await Database.InsertAsync(question);
+            }
+
+            return await Database.UpdateAsync(question);
+        }
+
+        public async Task<Question>? GetQuestion(int id)
+        {
+            if (id <= 0)
+            {
+                return null;
+            }
+
+            await Init();
+
+            // return the Question, otherwise return null on error.
+            try
+            {
+                return await Database.GetAsync<Question>(id);
+            } catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
