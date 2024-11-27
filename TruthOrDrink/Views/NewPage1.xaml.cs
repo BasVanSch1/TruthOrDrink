75bd83fce4b047579934c8ec5b4c54a5b9096644
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-11-27 20:28:08 +0100

    Styling practice

--- TruthOrDrink/Views/NewPage1.xaml.cs
+++ TruthOrDrink/Views/NewPage1.xaml.cs
@@ -1,10 +1,15 @@
+using TruthOrDrink.Data;
+using TruthOrDrink.Models;
+
 namespace TruthOrDrink.Views;
 
 public partial class NewPage1 : ContentPage
 {
-	public NewPage1()
+    private TruthOrDrinkDatabase _database;
+    public NewPage1(TruthOrDrinkDatabase db)
 	{
 		InitializeComponent();
+        _database = db;
 	}
 
     private async void HomeToolbarItem_Clicked(object sender, EventArgs e)
@@ -16,5 +21,52 @@ public partial class NewPage1 : ContentPage
     {
         await SecureStorage.SetAsync("IsAuthenticated", "false");
         await Shell.Current.GoToAsync("//login");
+    }
+
+    private async void GetQuestionsBtn_Clicked(object sender, EventArgs e)
+    {
+        var questions = await _database.GetQuestionsAsync();
+
+        string message = "Alle Vraag IDs:\n";
+
+        if (questions == null || questions.Count <= 0)
+        {
+            message = "Geen vragen in database";
+        } else
+        {
+            foreach (var question in questions)
+            {
+                message += $"{question.Id}\n";
+            }
+        }
+
+
+        await DisplayAlert("Vragen", message, "OK");
+    }
+
+    private async void CreateQuestionBtn_Clicked(object sender, EventArgs e)
+    {
+        var input = await DisplayPromptAsync("Vraag aanmaken", "Wat is de vraag?");
+
+        if (input == null)
+        {
+            await DisplayAlert("Alert", "Invalid input", "OK");
+            return;
+        }
+
+        var question = new Question()
+        {
+            Description = input,
+            QuestionType = QuestionType.Question,
+        };
+
+        // save a new question to the database if it doesn't exist, otherwise update the existing one. (in this case question has no ID so it will create a new one)
+        var result = await _database.UpdateQuestionAsync(question);
+
+        if (result == 0)
+        {
+            await DisplayAlert("ERROR", "Failed to create new question (0 rows affected)", "OK");
+            return;
+        }
     }
 }
