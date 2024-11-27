d3851e2e9234887044273c3346953d0c27575729
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-11-27 20:27:32 +0100

    Added primary key and other database attributes

---
+++ TruthOrDrink/Models/Question.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDrink.Models
+{
+    public class Question
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        [NotNull]
+        public string Description { get; set; }
+        [NotNull]
+        public QuestionType QuestionType { get; set; }
+    }
+
+    public enum QuestionType
+    {
+        Question,
+        Assignment,
+        Photo
+    }
+}
