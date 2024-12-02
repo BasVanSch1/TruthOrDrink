ec0767d68cc44f99b5744a3c854e665345f87548
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-12-02 12:24:13 +0100

    Database update

---
+++ TruthOrDrink/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace TruthOrDrink.Models
+{
+    internal class Profile
+    {
+        [PrimaryKey]
+        string Username { get; set; }
+        [NotNull]
+        string PasswordHash { get; set; }
+        [NotNull]
+        string DisplayName { get; set; }
+        List<Question>? PersonalQuestions { get; set; }
+        [NotNull]
+        int Truths { get; set; } = 0;
+        [NotNull]
+        int Drink { get; set; } = 0;
+        [NotNull]
+        int Games { get; set; } = 0;
+    }
+}
