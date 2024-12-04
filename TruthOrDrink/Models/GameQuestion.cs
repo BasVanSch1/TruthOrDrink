f5bebf92b00f4be6975d1a40862aa3a2fbc17cd2
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2024-12-04 18:10:33 +0100

    Added models

---
+++ TruthOrDrink/Models/GameQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDrink.Models
+{
+    internal class GameQuestion : Question
+    {
+        public Dictionary<Player, AnswerType> AnsweredBy { get; set; } = [];
+    }
+
+    public enum AnswerType
+    {
+        None,
+        Truth,
+        Drink
+    }
+}
---
+++ TruthOrDrink/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDrink.Models
+{
+    internal class Player
+    {
+        public string DisplayName { get; set; } = "Local Player";
+        public int Truths { get; set; } = 0;
+        public int Drinks { get; set; } = 0;
+        public string? ProfileUsername { get; set; }
+        public Profile? Profile { get; set; }
+
+        public void AnswerQuestion(AnswerType answer)
+        {
+            if (answer == AnswerType.Truth)
+            {
+                // TODO: add game logic
+                Truths++;
+            }
+            else if (answer == AnswerType.Drink)
+            {
+                // TODO: add game logic
+                Drinks++;
+            }
+        }
+    }
+}
