930ef8efcfd4fb12d9bda6a70f0de4baafd57f81
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-01-07 18:08:09 +0100

    Added FactAPI

---
+++ TruthOrDrink/Logic/FactLogic.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using TruthOrDrink.Models;
+
+namespace TruthOrDrink.Logic
+{
+    class FactLogic
+    {
+        public static async Task<string>? GetRandomFact()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(Constants.RANDOM_FACT_API);
+            var json = await response.Content.ReadAsStringAsync();
+            var fact = JsonConvert.DeserializeObject<Fact>(json);
+
+            return fact == null ? "Could not generate random fact" : fact.text;
+        }
+    }
+}
